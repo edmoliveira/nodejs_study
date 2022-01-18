45ca07bfb15097190dfdcf992884b361d07a0576
Edson Mendes de Oliveira <emoliveira29@hotmail.com>
2022-01-18 17:40:02 +0000

    modules

--- modules-project/app.js
+++ modules-project/app.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const log = require('./util.js');
+const validator = require('validator');
+const chalk = require('chalk');
 
 fs.appendFileSync("notes.txt", new Date().toString() + "\n");
 
-log('The text has been saved!');
+log(chalk.blue('The text has been saved!'));
+
+if(validator.isEmail('emoliveira@teste.com')){
+    log(chalk.green.bold('Email is valid!'));
+}
+else {
+    log(chalk.red('Email is not valid!'));
+}
